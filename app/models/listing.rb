42c19d8c5f4ae0ef1b084b02a2f7dd3b35c948bc
unnni <r.unnikrishnan@outlook.com>
2017-03-31 16:45:11 +0530

    added validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,7 @@
 class Listing < ApplicationRecord
 
 	  mount_uploader :image, ImageUploader
+	  validates :name, :description, :price , presence: true	
+	  validates :price , numericality: {greater_than: 0}
 end
 	
