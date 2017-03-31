ccb8b060f329c29111edba9faa3f12ab0ad344d1
unnni <r.unnikrishnan@outlook.com>
2017-03-31 17:05:22 +0530

    associated listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,5 +3,7 @@ class Listing < ApplicationRecord
 	  mount_uploader :image, ImageUploader
 	  validates :name, :description, :price , presence: true	
 	  validates :price , numericality: {greater_than: 0}
+
+	  belongs_to :user	
 end
 	
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170331104348) do
+ActiveRecord::Schema.define(version: 20170331111839) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170331104348) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
